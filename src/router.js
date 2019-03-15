89fc04fe1a297b30ad7f7dbd2caf437099097e04
Ihor S <ihor.sortynia@innovecs.com>
2019-03-15 20:08:32 +0200

    Add Product and 404 routes and pages;

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
+import NotFound from './views/404'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -20,6 +21,15 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: () => import(/* webpackChunkName: "product" */ './views/Product.vue')
+    },
+    {
+      path: '*',
+      component: NotFound
     }
   ]
 })
