5909d3821a5b0f69e18cd14b531f0ee510db84e0
Ihor S <ihor.sortynia@innovecs.com>
2019-03-15 20:31:46 +0200

    Add image to Product page and use binding for title output;

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="product">
-        <h1>This is a product page</h1>
+        <section class="product__image">
+            <img src="@/assets/logo.png" :alt="title" />
+        </section>
+
+        <section class="product__info">
+            <h1>{{title}}</h1>
+        </section>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'product',
+        data: () => ({
+            title: 'Socks'
+        })
+    }
+</script>
+
