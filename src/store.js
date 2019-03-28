eb5d6c18e6ae406885ff82431d544a863ae45b1b
Ihor <ihor.sortynia@innovecs.com>
2019-03-28 17:50:12 +0200

    Start to work with state and getters from Vuex;

--- src/store.js
+++ src/store.js
@@ -1,16 +1,68 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        user: {
+            id: '1',
+            name: 'Ihor Gevorkyan',
+            role: 'admin'
+        },
+        categories: [
+            'sustainability',
+            'nature',
+            'animal welfare',
+            'housing',
+            'education',
+            'food',
+            'community'
+        ],
+        events: [
+            { id: 1, title: 'Event #1', organizer: 'Ihor Gevorkyan' },
+            { id: 2, title: 'Event #2', organizer: 'Ms. Smith' },
+            { id: 3, title: 'Event #3', organizer: 'Ihor Gevorkyan' }
+        ]/* ,
+        todos: [
+            {
+                id: 1,
+                text: 'Todo #1',
+                done: false
+            },
+            {
+                id: 2,
+                text: 'Todo #2',
+                done: true
+            },
+            {
+                id: 3,
+                text: 'Todo #3',
+                done: true
+            },
+            {
+                id: 4,
+                text: 'Todo #4',
+                done: false
+            },
+            {
+                id: 5,
+                text: 'Todo #5',
+                done: true
+            },
+            {
+                id: 6,
+                text: 'Todo #6',
+                done: false
+            }
+        ] */
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+        categoriesLength: state => state.categories.length,
+        getEventByID: state => id => state.events.find(event => event.id === id),
+        // doneTodos: state => state.todos.filter(todo => todo.done),
+        // activeTodosCount: (state, getters) => state.todos.length - getters.doneTodos.length - example with using getters in getter
+    }
 })
--- src/views/events/views/EventForm.vue
+++ src/views/events/views/EventForm.vue
@@ -1,5 +1,41 @@
 <template>
     <div>
-        <h2>Create Event</h2>
+        <h2>Create Event, {{ username }}</h2> <!-- {{ $store.state.user.name }} -->
+
+        <p class="h-text-secondary">
+            There are {{ categoriesLength }} categories
+        </p>
+
+        <ul v-if="categoriesLength">
+            <li v-for="(category, index) in categories" :key="index">
+                <span>{{ category }}</span>
+            </li>
+        </ul>
+
+        <p>{{ getEvent(2) }}</p>
     </div>
 </template>
+
+<script>
+    import { mapState, mapGetters } from 'vuex'
+
+    export default {
+        computed: {
+            // getEvent() {
+            //     return this.$store.getters.getEventByID
+            // },
+            // categoriesLength() {
+            //     return this.$store.getters.categoriesLength
+            // },
+            ...mapGetters({
+                getEvent: 'getEventByID',
+                categoriesLength: 'categoriesLength'
+            }),
+            ...mapState({
+                username: state => state.user.name,
+                categories: 'categories'
+            })
+            // mapState(['user', 'categories']) - if you need all user data and not need custom names of entities in component
+        }
+    }
+</script>
